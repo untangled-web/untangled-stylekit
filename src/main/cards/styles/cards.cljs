83038ad9dae160f927e28e02e53b67a54ef028e5
Tony Kay <twkay@thenavisway.com>
2016-12-08 14:54:05 -0800

    added docstrings that make sense

--- src/main/cards/styles/cards.cljs
+++ src/main/cards/styles/cards.cljs
@@ -13,19 +13,19 @@
 
 ;; Sample Example
 (defexample badge-example-1
-  "# Boo ya "
+  "## A plain badge"
   (dom/p #js {}
     (dom/a #js {:href "#"} "Inbox "
       (dom/span #js {:className "c-badge"} 7))))
 
 (defexample badge-on-button
-  "DOC \"asdasd\""
+  "## A Badge on a Button"
   (dom/p #js {}
     (dom/button #js {:className "c-button"} " Messages "
       (dom/span #js {:className "c-badge"} "37"))))
 
 (defexample badge-with-icon
-  "DOC \"asdasd\""
+  "## A Badge with an Icon"
   (dom/span #js {:className "c-badge c-badge--round"}
     (dom/span #js {:className "c-icon"}
       (dom/svg #js {:xmlns "http://www.w3.org/2000/svg" :width "24" :height "24" :viewBox "0 0 24 24"}
@@ -35,13 +35,13 @@
                            0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z"})))))
 
 (defexample button-state
-  "DOC \"asdasd\""
+  "## Button States"
   (dom/div #js {}
     (dom/button #js {:aria-disabled "true" :className "c-button is-disabled"} "Disabled")
     (dom/button #js {:title "Click me to see the active state." :className "c-button is-active"} "Active")))
 
 (defexample button-size-and-shape
-  "DOC \"asdasd\""
+  "## Button size and shape"
   (dom/div #js {}
     (dom/button #js {:className "c-button"} "Regular")
     (dom/button #js {:className "c-button c-button--large"} "Large")
@@ -64,7 +64,7 @@
           (dom/path #js {:d "M12.87 15.07l-2.54-2.51.03-.03c1.74-1.94 2.98-4.17 3.71-6.53H17V4h-7V2H8v2H1v1.99h11.17C11.5 7.92 10.44 9.75 9 11.35 8.07 10.32 7.3 9.19 6.69 8h-2c.73 1.63 1.73 3.17 2.98 4.56l-5.09 5.02L4 19l5-5 3.11 3.11.76-2.04zM18.5 10h-2L12 22h2l1.12-3h4.75L21 22h2l-4.5-12zm-2.62 7l1.62-4.33L19.12 17h-3.24z"}))))))
 
 (defexample button-color
-  "DOC \"asdasd\""
+  "## Button Color"
   (dom/div #js {}
     (dom/button #js {:className "c-button"} "Default")
     (dom/button #js {:className "c-button c-button--secondary"} "Secondary")
