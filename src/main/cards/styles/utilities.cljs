a129314699cfff64e2dc1c56995cb21adfb0bce3
Scott Paulson <sgpaulson@thenavisway.com>
2016-12-09 16:07:43 -0800

    Reworking grid examples a bit to get some markup added in using headers. Preparing for rebase.

--- src/main/cards/styles/utilities.cljs
+++ src/main/cards/styles/utilities.cljs
@@ -11,7 +11,13 @@
 ;; START OF EXAMPLES
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Sample Example
+;; Grid Examples
+
+(def grid-header (devcards.core/markdown->react "Grids are the bread and butter of web design, this one is based on flexbox. Needless to say you will find this grid system a joy to use. [Flexbox Mythbusting](http://jonyablonski.com/2015/flexbox-myth-busting/)\n\n
+#### New to grids?\n\n
+Check out my presentation on CSS Grids can help you, complete with a [slide deck](https://speakerdeck.com/stephenway/css-grids-can-help-you), and a [CodePen](http://codepen.io/stephenway/pen/dMKzvy) to play with complex grid layouts."))
+
+
 (defexample grid-example-1
   "## A 12 column grid"
   (dom/div #js {:className "u-row"}
@@ -40,8 +46,11 @@
            (dom/div #js {:className "u-column--1"}
                     (dom/div #js {:className "box-row box-tall"} "12"))))
 
-(defexample grid-example-2
-  "## Learn"
+(def learn-header (devcards.core/markdown->react "This group of select boxes will show you how columns adapt to their siblings when they occupy more or less columns."))
+
+;TODO: learn switchers are not working as included here. Need to add actions.
+(defexample learn-example
+  ""                                                        ;"## Learn"
   (dom/div #js {:className "u-row o-column-switcher-row"}
            (dom/div #js {:className "u-column--4"}
                     (dom/select #js {:className "o-column-switcher"}
@@ -331,12 +340,10 @@
                     (dom/div #js {:className "u-column--11 u-pull--1"}
                              (dom/div #js {:className "box-row"}
                                       (dom/span #js {}
-                                                (dom/i #js {:className "c-icon-right-dir"}) " 1")))))
-  )
+                                                (dom/i #js {:className "c-icon-right-dir"}) " 1"))))))
 
 (defexample grid-example-8
   "## Column Push/Pull"
-
   (dom/div #js {:className "u-row"}
            (dom/div #js {:className "u-column--2 u-push--5 u-pull--5"}
                     (dom/div #js {:className "box-row"} ".u-column--2"
@@ -363,9 +370,9 @@
                                                         (dom/div #js {:className "box-first box-container"}
                                                                  (dom/div #js {:className "u-row"}
                                                                           (dom/div #js {:className "u-column--6"}
-                                                                                   (dom/div #js {:className "box-nested"}) " ")
+                                                                                   (dom/div #js {:className "box-nested"}))
                                                                           (dom/div #js {:className "u-column--6"}
-                                                                                   (dom/div #js {:className "box-nested"}) " ") " ") " ") " ") " ") " ") " ")
+                                                                                   (dom/div #js {:className "box-nested"}))))))))
                     (dom/div #js {:className "u-column--8"}
                              (dom/div #js {:className "box box-container"}
                                       (dom/div #js {:className "u-row"}
@@ -373,12 +380,11 @@
                                                         (dom/div #js {:className "box-first box-container"}
                                                                  (dom/div #js {:className "u-row"}
                                                                           (dom/div #js {:className "u-column--3"}
-                                                                                   (dom/div #js {:className "box-nested"}) " ")
+                                                                                   (dom/div #js {:className "box-nested"}))
                                                                           (dom/div #js {:className "u-column--6"}
-                                                                                   (dom/div #js {:className "box-nested"}) " ")
+                                                                                   (dom/div #js {:className "box-nested"}))
                                                                           (dom/div #js {:className "u-column--3"}
-                                                                                   (dom/div #js {:className "box-nested"}) " ") " ") " ") " ") " ") " ") " ") " "))
-  )
+                                                                                   (dom/div #js {:className "box-nested"})))))))))))
 
 (defexample grid-example-align-start
   "## Align - Start"
@@ -387,8 +393,7 @@
                     (dom/div #js {:className "box box-container"}
                              (dom/div #js {:className "u-row u-start"}
                                       (dom/div #js {:className "u-column--6"}
-                                               (dom/div #js {:className "box-nested"} " .u-row.u-start "))))))
-  )
+                                               (dom/div #js {:className "box-nested"} " .u-row.u-start ")))))))
 
 (defexample grid-example-align-center
   "## Align - Center"
@@ -397,8 +402,7 @@
                     (dom/div #js {:className "box box-container"}
                              (dom/div #js {:className "u-row u-center"}
                                       (dom/div #js {:className "u-column--8"}
-                                               (dom/div #js {:className "box-nested"} " .u-row.u-center "))))))
-  )
+                                               (dom/div #js {:className "box-nested"} " .u-row.u-center ")))))))
 
 (defexample grid-example-align-end
   "## Align - End"
@@ -407,8 +411,7 @@
                     (dom/div #js {:className "box box-container"}
                              (dom/div #js {:className "u-row u-end"}
                                       (dom/div #js {:className "u-column--4"}
-                                               (dom/div #js {:className "box-nested"} " .u-row.u-end "))))))
-  )
+                                               (dom/div #js {:className "box-nested"} " .u-row.u-end ")))))))
 
 (defexample grid-example-align-top
   "## Align - Top"
@@ -416,8 +419,7 @@
            (dom/div #js {:className "u-column--6"}
                     (dom/div #js {:className "box--large"} ".u-row.u-top"))
            (dom/div #js {:className "u-column--6"}
-                    (dom/div #js {:className "box"})))
-  )
+                    (dom/div #js {:className "box"}))))
 
 (defexample grid-example-align-middle
   "## Align - Middle"
@@ -425,8 +427,7 @@
            (dom/div #js {:className "u-column--6"}
                     (dom/div #js {:className "box--large"} ".u-row.u-middle"))
            (dom/div #js {:className "u-column--6"}
-                    (dom/div #js {:className "box"})))
-  )
+                    (dom/div #js {:className "box"}))))
 
 (defexample grid-example-align-bottom
   "## Align - Bottom"
@@ -434,12 +435,10 @@
            (dom/div #js {:className "u-column--6"}
                     (dom/div #js {:className "box--large"} ".u-row.u-bottom"))
            (dom/div #js {:className "u-column--6"}
-                    (dom/div #js {:className "box"})))
-  )
+                    (dom/div #js {:className "box"}))))
 
 (defexample grid-example-distributed-around
   "## Distributed - Around"
-
   (dom/div #js {:className "u-row"}
            (dom/div #js {:className "u-column--12"}
                     (dom/div #js {:className "box box-container"}
@@ -449,12 +448,10 @@
                                       (dom/div #js {:className "u-column--2"}
                                                (dom/div #js {:className "box-nested"} "2"))
                                       (dom/div #js {:className "u-column--2"}
-                                               (dom/div #js {:className "box-nested"} "2"))))))
-  )
+                                               (dom/div #js {:className "box-nested"} "2")))))))
 
 (defexample grid-example-distributed-between
   "## Distributed - Between"
-
   (dom/div #js {:className "u-row"}
            (dom/div #js {:className "u-column--12"}
                     (dom/div #js {:className "box box-container"}
@@ -464,12 +461,10 @@
                                       (dom/div #js {:className "u-column--2"}
                                                (dom/div #js {:className "box-nested"} "2"))
                                       (dom/div #js {:className "u-column--2"}
-                                               (dom/div #js {:className "box-nested"} "2"))))))
-  )
+                                               (dom/div #js {:className "box-nested"} "2")))))))
 
 (defexample grid-example-ordering-first
   "## Ordering - First"
-
   (dom/div #js {:className "u-row"}
            (dom/div #js {:className "u-column--12"}
                     (dom/div #js {:className "box box-container"}
@@ -485,12 +480,10 @@
                                       (dom/div #js {:className "u-column--2"}
                                                (dom/div #js {:className "box-first"} "5"))
                                       (dom/div #js {:className "u-column--2 u-first"}
-                                               (dom/div #js {:className "box-nested"} "6"))))))
-  )
+                                               (dom/div #js {:className "box-nested"} "6")))))))
 
 (defexample grid-example-ordering-last
   "## Ordering - Last"
-
   (dom/div #js {:className "u-row"}
            (dom/div #js {:className "u-column--12"}
                     (dom/div #js {:className "box box-container"}
@@ -506,12 +499,10 @@
                                       (dom/div #js {:className "u-column--2"}
                                                (dom/div #js {:className "box-first"} "5"))
                                       (dom/div #js {:className "u-column--2"}
-                                               (dom/div #js {:className "box-first"} "6"))))))
-  )
+                                               (dom/div #js {:className "box-first"} "6")))))))
 
 (defexample grid-example-ordering-reverse
   "## Ordering - Reverse"
-
   (dom/div #js {:className "u-row"}
            (dom/div #js {:className "u-column--12"}
                     (dom/div #js {:className "box box-container"}
@@ -527,35 +518,89 @@
                                       (dom/div #js {:className "u-column--2"}
                                                (dom/div #js {:className "box-first"} "5"))
                                       (dom/div #js {:className "u-column--2"}
-                                               (dom/div #js {:className "box-first"} "6"))))))
-  )
+                                               (dom/div #js {:className "box-first"} "6")))))))
+
+;TODO: What can we do something with these css "@media" items?
+(defexample media-example-breakpoints-flexible
+  "## Media - Flexible Breakpoints"
+  (dom/div nil nil))
+
+(defexample media-example-breakpoints-fixed
+  "## Media - Fixed Breakpoints"
+  (dom/div nil nil))
+
+(defexample media-example-breakpoints-orientation
+  "## Media - Orientation Breakpoints"
+  (dom/div nil nil))
+
+(defexample media-example-breakpoints-media-type
+  "## Media - Media Type Breakpoints"
+  (dom/div nil nil))
+
+;; Positioning Examples
+
+(defexample positioning-example-fixed
+  "## Positioning - Fixed Classes"
+  (dom/div #js {}
+           (dom/div #js {:className "u-fixed--top"})
+           (dom/div #js {:className "u-fixed--top-center"})
+           (dom/div #js {:className "u-fixed--top-right"})
+           (dom/div #js {:className "u-fixed--top-left"})
+           (dom/div #js {:className "u-fixed--bottom"})
+           (dom/div #js {:className "u-fixed--bottom-right"})
+           (dom/div #js {:className "u-fixed--bottom-left"})
+           (dom/div #js {:className "u-fixed--middle"})
+           (dom/div #js {:className "u-fixed--middle-center"})))
+
+(defexample positioning-example-absolute
+  "## Positioning - Absolute Classes"
+(dom/div #js {}
+(dom/div #js {:className "u-absolute--top"})
+(dom/div #js {:className "u-absolute--top-right"})
+(dom/div #js {:className "u-absolute--top-left"})
+(dom/div #js {:className "u-absolute--bottom"})
+(dom/div #js {:className "u-absolute--bottom-right"})
+(dom/div #js {:className "u-absolute--bottom-left"})
+(dom/div #js {:className "u-absolute--middle"})
+(dom/div #js {:className "u-absolute--middle-center"})))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; START OF SECTIONS (within a feature set...e.g. components)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; NOTE: This is where you add the sections for index
+
+;TODO: Need to determine how markup should be added if Stephen's layout with text under the titles and multiple
+; levels are desired. The below shows a couple ideas based upon the "header" idea, but this isn't a good match so
+; I'm waiting to modify the rest until we decide our target.
+
 (def sections [
                ; NOTE: :examples is a list of example names, rendered in order given
-               {:id :grids :title "Grid" :examples [grid-example-1 grid-example-2 grid-example-3 grid-example-4
-                                                    grid-example-5 grid-example-6 grid-example-7 grid-example-8
-                                                    grid-example-9
-                                                    grid-example-align-start grid-example-align-center
-                                                    grid-example-align-end grid-example-align-top
-                                                    grid-example-align-middle grid-example-align-bottom
-                                                    grid-example-distributed-around grid-example-distributed-between
-                                                    grid-example-ordering-first grid-example-ordering-last
-                                                    grid-example-ordering-reverse]}
+               {:id :grids :title "Grid" :header grid-header :examples [grid-example-1]}
+               {:id :grids-learn :title "Learn" :header learn-header :examples [learn-example
+                                                                                grid-example-3 grid-example-4
+                                                                                grid-example-5 grid-example-6 grid-example-7 grid-example-8
+                                                                                grid-example-9
+                                                                                grid-example-align-start grid-example-align-center
+                                                                                grid-example-align-end grid-example-align-top
+                                                                                grid-example-align-middle grid-example-align-bottom
+                                                                                grid-example-distributed-around grid-example-distributed-between
+                                                                                grid-example-ordering-first grid-example-ordering-last
+                                                                                grid-example-ordering-reverse]}
+               {:id :positioning :title "Positioning" :header nil :examples [positioning-example-fixed
+                                                                       positioning-example-absolute]}
                ])
 
 ;; NOTE: How to render a section (with all examples) including hyperlink target anchor
 (defn section
   [id sections]
-  (let [{:keys [title examples]} (first (filter #(= id (:id %)) sections))]
+  (let [{:keys [title examples header]} (first (filter #(= id (:id %)) sections))]
     (dom/div nil
              (dom/a #js {:id id}
                     (dom/h1 nil title))
+             (dom/p nil header)
              (map #(%) examples))))
+
 
 ;; NOTE: Rendering of the clickable index
 (defn section-index [sections]
@@ -569,6 +614,8 @@
     (dom/div nil
              (section-index sections)
              (section :grids sections)
+             (section :grids-learn sections)
+             (section :positioning sections)
              )))
 
 
